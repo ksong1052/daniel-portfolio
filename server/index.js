6ac5ccea68cf16c0361457208cd5f08657666808
ksong1052 <ksong1052@gmail.com>
2021-11-17 20:32:03 -0400

    Complete to configure for server with REST API

--- server/index.js
+++ server/index.js
@@ -5,6 +5,9 @@ const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const multer = require("multer");
 
 dotenv.config();
 
@@ -19,9 +22,30 @@ mongoose.connect(process.env.MONGO_URL, {
 .then(console.log("Connected to MongoDB..."))
 .catch((err) => console.log(err));
 
+// Multer: 이미지 업로드를 위한  Library
+// 이미지 업로드는 client side에서 설정하는 부분이 필요하다.
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  }, 
+  filename: (req, file, cb) => {
+    //cb(null, "myimage.png");
+    cb(null, req.body.name);
+  }
+});
+
+const upload = multer({storage:storage});
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded...!!!");
+});
+
+
 /* Getting Router */ 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
+
 
 /* Using port to connect Server */ 
 app.listen(PORT, ()=> {
